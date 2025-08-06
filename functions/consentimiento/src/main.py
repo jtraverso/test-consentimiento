0767c7c02bc3bfef0cb5896ba1c30a81357c349c
Juan Pablo Traverso <jtraverso@users.noreply.github.com>
2025-08-06 20:18:26 +0200

    Update main.py

--- functions/consentimiento/src/main.py
+++ functions/consentimiento/src/main.py
@@ -1,39 +1,67 @@
-from appwrite.client import Client
-from appwrite.services.users import Users
-from appwrite.exception import AppwriteException
-import os
+import json
+import requests
 
-# This Appwrite function will be executed every time your function is triggered
 def main(context):
-    # You can use the Appwrite SDK to interact with other services
-    # For this example, we're using the Users service
-    client = (
-        Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
-    )
-    users = Users(client)
-
-    try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
-
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+    # Configuración
+    API_BASE = "https://demos2.vu-one.com"
+    API_KEY = "17532252-8942-3036-v5eR-YRy5dV5OtxYK"
+    JSESSIONID = "D8D3DC4396DC313EEE1601EE2F8B6B2F"
+    
+    headers = {
+        "Content-Type": "application/json",
+        "x-api-key": API_KEY
+    }
+    cookies = {"JSESSIONID": JSESSIONID}
+    
+    method = context.req.method
+    
+    if method == "GET":
+        # 1. Trae los datos del consentimiento por ID
+        template_id = context.req.query.get("id", "2")
+        r1 = requests.get(
+            f"{API_BASE}/consent/consentTemplate/getConsentTemplateById?id={template_id}",
+            headers=headers, cookies=cookies
+        )
+        if r1.status_code != 200:
+            return context.res.json(
+                {"error": "Error obteniendo consentimiento", "details": r1.text},
+                status_code=500
+            )
+        data = r1.json()
+        
+        # 2. Trae el legalText por name/version fijo o los retornados
+        name = "CT-TOS-TANNER-CL"
+        version = "1.0.1"
+        r2 = requests.get(
+            f"{API_BASE}/consent/consentTemplate/getLegalTextByNameAndVersion?name={name}&version={version}",
+            headers=headers, cookies=cookies
+        )
+        if r2.status_code != 200:
+            legal_text = None
+        else:
+            legal_text = r2.json().get("legalText")
+        
+        # 3. Combina los datos y retorna todo junto
+        data["legalText"] = legal_text
+        return context.res.json(data)
+    
+    elif method == "POST":
+        # Guarda el consentimiento
+        body = context.req.body.decode() if isinstance(context.req.body, bytes) else context.req.body
+        try:
+            data = json.loads(body)
+        except Exception as e:
+            return context.res.json({"error": "JSON inválido", "details": str(e)}, status_code=400)
+        r = requests.post(
+            f"{API_BASE}/consent/consentUser/save",
+            headers=headers, cookies=cookies, json=data
+        )
+        if r.status_code != 200:
+            return context.res.json(
+                {"error": "Error guardando consentimiento", "details": r.text},
+                status_code=500
+            )
+        return context.res.json(r.json())
+    
+    else:
+        return context.res.json({"error": "Método no soportado"}, status_code=405)
